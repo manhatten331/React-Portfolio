38de5ea92f18b40cf6383008bc0f94dd4d5d6d81
Manzell <manzellhatten@gmail.com>
2019-07-04 22:50:29 -0400

    Starting

---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+
+function Header() {
+    return (
+        <div>
+            <div className="Header">
+            </div>
+
+            <div className="Content">
+                <h1>About me</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
